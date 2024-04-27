cb7f04ca7620c5e1a8f3b18de27ff10b935e8c64
Daniel Xiong <daniel@daniels-mbp-6.devices.brown.edu>
2024-04-27 17:56:26 -0400

    updated build graph, now creates word embeddings and builds adjacency matrix

--- build_graph.py
+++ build_graph.py
@@ -2,8 +2,10 @@ import scipy.sparse as sp
 import tensorflow as tf
 from preprocess import *
 #preprocess
-def build_graph(path,window_size):
-    'builds a graph, takes in path to data and window size, and returns adjacency matrix, node_features, edge_features'
+def build_graph(path,window_size, embedding_dim ):
+    '''builds a graph, takes in path to data, window size, wordembedding dim, 
+    and returns adjacency matrix (vocabsize,vocabsize), word_vectors (vocab_size, embedding_dim), edge_features (
+    not implemented yet)'''
     
     inputs, labels = preprocess(path)    
     #build adjacency matrix. Words are already converted to a corresponding number in preprocess
@@ -17,9 +19,9 @@ def build_graph(path,window_size):
                 adj_matrix = tf.tensor_scatter_nd_add(adj_matrix, 
                                                    indices=[[cur_word, word], [word, cur_word]], 
                                                    updates=[1, 1])
+    word_vectors = tf.random.uniform(-0.01, 0.01, (VOCAB_SIZE,embedding_dim))
 
-
-    return tf.sparse.from_dense(adj_matrix),
+    return tf.sparse.from_dense(adj_matrix),word_vectors
 
 
 build_graph('data/movie_review.parquet', 20)
