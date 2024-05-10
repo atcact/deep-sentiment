59b4cd8638abbc896d529b1a842b598bbf84485b
atcact <caothuyan15112004@gmail.com>
2024-05-09 23:00:42 -0400

    update params

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import pandas as pd
 
 
 from preprocess import preprocess, train_test_split, shuffle
-from preprocess1 import preprocess
 from models.rnn.models import RNN
 from models.am_gcn.models import AMGCN
 from models.mc_cnn.models import MC_CNN
@@ -18,45 +17,44 @@ from models.mc_cnn.models import MC_CNN
 def parseArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="rnn")
-    parser.add_argument("--batch_size", type=int, default=128)
-    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--epochs", type=int, default=3)
+    parser.add_argument("--dataset", type=str, default="imdb")
+    parser.add_argument("--dropout", type=float, default=0.5)
     args = parser.parse_args()
     return args
 
 
 def main(args):
-    inputs, labels = preprocess('data/movie_review.parquet')
-    
-    #df = pd.read_csv("data/sts_gold_tweet.csv")
-    #df.to_parquet("data/sts_gold_tweet.parquet")
-
-    #inputs, labels = preprocess('data/sts_gold_tweet.parquet')
-
+    input_length = 100
+    if args.dataset == "imdb":
+        inputs, labels = preprocess('data/movie_review.parquet', input_length=input_length)
+    elif args.dataset == "sts_gold":
+        input_length = 10
+        inputs, labels = preprocess('data/sts_gold_tweet.parquet', input_length=input_length, process_text=True) 
     
     train_inputs, test_inputs, train_labels, test_labels = train_test_split(inputs, labels, test_size=0.2)
     if args.model == "rnn":
-        model = RNN()
+        model = RNN(input_length=input_length)
     elif args.model == "amgcn":
-        model = AMGCN(nfeat=300, nclass=2, nhid1=256, nhid2=128, n=2, dropout=0.5)
-    # TODO: Add more models
+        model = AMGCN(nfeat=300, nclass=2, nhid1=256, nhid2=128, n=2, dropout=args.dropout)
     elif args.model == "mccnn":
-        model = MC_CNN(length=100, vocab_size=20000)
+        model = MC_CNN(length=input_length, vocab_size=20000, dropout_rate=args.dropout)
     else:
         raise ValueError("Invalid model name")
     
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    model.save('model_' + args.model + '.h5')
+    # model.save('model_' + args.model + '.h5')
     # Train & evaluate the model
     if args.model == "mccnn":
-        model.fit([train_inputs, train_inputs, train_inputs], train_labels, batch_size=8, epochs=3) # Update parameters
+        model.fit([train_inputs, train_inputs, train_inputs], train_labels, batch_size=args.batch_size, epochs=args.epochs) # Update parameters
         loss, accuracy = model.evaluate([test_inputs, test_inputs, test_inputs], test_labels)
     else:
-        model.fit(train_inputs, train_labels, batch_size=8, epochs=3)
+        model.fit(train_inputs, train_labels, batch_size=args.batch_size, epochs=args.epochs)
         loss, accuracy = model.evaluate(test_inputs, test_labels)
         
     print(f'Test loss: {loss:.3f}, Test accuracy: {accuracy:.3f}')
-    # train(model, train_inputs, train_labels, args.batch_size, args.num_epochs)
-    # evaluate(model, test_inputs, test_labels)
+
     
 if __name__ == "__main__":
     args = parseArguments()
--- models/mc_cnn/models.py
+++ models/mc_cnn/models.py
@@ -2,26 +2,26 @@ import tensorflow as tf
 import numpy as np
 from keras.models import Model
 
-def MC_CNN(length, vocab_size):
+def MC_CNN(length, vocab_size, dropout_rate=0.5):
     # channel 1
         inputs1 = tf.keras.layers.Input(shape=(length,))
         embedding1 = tf.keras.layers.Embedding(vocab_size, 100)(inputs1)
         conv1 = tf.keras.layers.Conv1D(filters=16, kernel_size=4, activation='relu')(embedding1)
-        drop1 = tf.keras.layers.Dropout(0.6)(conv1)
+        drop1 = tf.keras.layers.Dropout(dropout_rate)(conv1)
         pool1 = tf.keras.layers.MaxPooling1D(pool_size=2)(drop1)
         flat1 = tf.keras.layers.Flatten()(pool1)
         # channel 2
         inputs2 = tf.keras.layers.Input(shape=(length,))
         embedding2 = tf.keras.layers.Embedding(vocab_size, 100)(inputs2)
         conv2 = tf.keras.layers.Conv1D(filters=32, kernel_size=6, activation='relu')(embedding2)
-        drop2 = tf.keras.layers.Dropout(0.7)(conv2)
+        drop2 = tf.keras.layers.Dropout(dropout_rate)(conv2)
         pool2 = tf.keras.layers.MaxPooling1D(pool_size=2)(drop2)
         flat2 = tf.keras.layers.Flatten()(pool2)
         # channel 3
         inputs3 = tf.keras.layers.Input(shape=(length,))
         embedding3 = tf.keras.layers.Embedding(vocab_size, 100)(inputs3)
         conv3 = tf.keras.layers.Conv1D(filters=16, kernel_size=8, activation='relu')(embedding3)
-        drop3 = tf.keras.layers.Dropout(0.7)(conv3)
+        drop3 = tf.keras.layers.Dropout(dropout_rate)(conv3)
         pool3 = tf.keras.layers.MaxPooling1D(pool_size=2)(drop3)
         flat3 = tf.keras.layers.Flatten()(pool3)
         # merge
